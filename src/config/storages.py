9c5371273143a25c8f1ae289197eca05f31df41e
Khramenko Vladislav Vladimirovich <vladhromenko@gmail.com>
2025-06-03 11:15:38 +0300

    fixes

--- src/config/storages.py
+++ src/config/storages.py
@@ -1,12 +1,12 @@
 from storages.backends.s3boto3 import S3Boto3Storage
 
 class MinIOMediaStorage(S3Boto3Storage):
-    bucket_name = 'local-bucket-shop'
+    bucket_name = 'local-bucket-form'
     location = 'media'
     file_overwrite = False
     custom_domain = False
 
 class StaticStorage(S3Boto3Storage):
-    bucket_name = 'local-bucket-shop'
+    bucket_name = 'local-bucket-form'
     location = "static"
     default_acl = None
